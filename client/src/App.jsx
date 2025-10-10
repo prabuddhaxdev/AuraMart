7a1815a77b2c84f8943d38a9c24b1056621a79b6
prabuddhaxdev <prabuddha1749@gmail.com>
2025-10-10 23:56:43 +0530

    Add Shopping View pages and layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,10 @@ import AdminProducts from "./pages/admin-view/products";
 import AdminOrders from "./pages/admin-view/orders";
 import AdminFeatures from "./pages/admin-view/features";
 import ShoppingLayout from "./components/shopping-view/layout";
+import ShoppingHome from "./pages/shopping-view/home";
+import ShoppingListing from "./pages/shopping-view/listing";
+import ShoppingCheckout from "./pages/shopping-view/checkout";
+import ShoppingAccount from "./pages/shopping-view/account";
 import NotFound from "./pages/not-found";
 
 
@@ -24,7 +28,6 @@ function App() {
           <Route path="register" element={<AuthRegister />} />
         </Route>
 
-
         <Route path="/admin" element={<AdminLayout />}>
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="products" element={<AdminProducts />} />
@@ -32,11 +35,13 @@ function App() {
           <Route path="features" element={<AdminFeatures />} />
         </Route>
 
-
-        <Route path="/shop" element={<ShoppingLayout/>}></Route>
-        <Route path="*" element={<NotFound/>}/>
-
-
+        <Route path="/shop" element={<ShoppingLayout />}>
+          <Route path="home" element={<ShoppingHome />} />
+          <Route path="listing" element={<ShoppingListing/>} />
+          <Route path="checkout" element={<ShoppingCheckout/>} />
+          <Route path="account" element={<ShoppingAccount/>} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
