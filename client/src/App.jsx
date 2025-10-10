63776496c2a28d0a5782d6500df9a761615fe841
prabuddhaxdev <prabuddha1749@gmail.com>
2025-10-10 22:31:18 +0530

    Add Not Found page and Shopping View pages and layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,9 @@ import AdminDashboard from "./pages/admin-view/dashboard";
 import AdminProducts from "./pages/admin-view/products";
 import AdminOrders from "./pages/admin-view/orders";
 import AdminFeatures from "./pages/admin-view/features";
+import ShoppingLayout from "./components/shopping-view/layout";
+import NotFound from "./pages/not-found";
+
 
 function App() {
 
@@ -29,6 +32,9 @@ function App() {
           <Route path="features" element={<AdminFeatures />} />
         </Route>
 
+
+        <Route path="/shop" element={<ShoppingLayout/>}></Route>
+        <Route path="*" element={<NotFound/>}/>
 
 
       </Routes>
---
+++ client/src/pages/not-found/index.jsx
@@ -0,0 +1,5 @@
+function NotFound() {
+  return <div>Oops! The page you are looking for does not exist.</div>;
+}
+
+export default NotFound;
