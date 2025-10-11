bc6dedd4ecf06f3cdabbe3f22bc519d54ddfa1b9
prabuddhaxdev <prabuddha1749@gmail.com>
2025-10-12 00:00:31 +0530

    Created basic login and register pages

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const AuthLogin = () => {
   return (
-    <div>AuthLogin</div>
-  )
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Sign in to your account
+        </h1>
+        <p className="mt-2">
+          Don't have an account
+          <Link
+            className="font-medium ml-2 text-primary hover:underline"
+            to="/auth/register"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default AuthLogin
--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const AuthRegister = () => {
   return (
-    <div>
-    Auth Register
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Create new account
+        </h1>
+        <p className="mt-2">
+          Already have an account
+          <Link
+            className="font-medium ml-2 text-primary hover:underline"
+            to="/auth/login"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   )
 }
