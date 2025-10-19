5426508806d9bc5f57391869483954506094e689
prabuddhaxdev <prabuddha1749@gmail.com>
2025-10-19 23:47:54 +0530

    Add Shopping cart routes logic

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const authRouter = require("./routes/auth/auth.routes");
 const adminProductsRouter = require("./routes/admin/products.routes");
 
 const shopProductsRouter = require("./routes/shop/products.routes");
+const shopCartRouter = require("./routes/shop/cart.routes");
 
 require("dotenv").config();
 
@@ -39,6 +40,6 @@ app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 
 app.use("/api/shop/products", shopProductsRouter);
-
+app.use("/api/shop/cart", shopCartRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
