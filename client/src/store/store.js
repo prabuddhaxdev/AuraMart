979dd0dbfe91f9e56fae8ad75cbe7c7568433d9d
prabuddhaxdev <prabuddha1749@gmail.com>
2025-10-22 02:08:18 +0530

    Implemented product review

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -7,6 +7,7 @@ import shopProductsSlice from "./shop/products-slice";
 import shopCartSlice from "./shop/cart-slice";
 import shopAddressSlice from "./shop/address-slice";
 import shopOrderSlice from "./shop/order-slice";
+import shopReviewSlice from "./shop/review-slice";
 
 const store = configureStore({
   reducer: {
@@ -19,6 +20,9 @@ const store = configureStore({
     shopCart: shopCartSlice,
     shopAddress: shopAddressSlice,
     shopOrder: shopOrderSlice,
+    shopReview: shopReviewSlice,
+
+    
   },
 });
 
--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const shopCartRouter = require("./routes/shop/cart.routes");
 const shopAddressRouter = require("./routes/shop/address.routes");
 const shopOrderRouter = require("./routes/shop/order.routes");
 const shopSearchRouter = require("./routes/shop/search.routes");
+const shopReviewRouter = require("./routes/shop/review.routes");
 
 
 require("dotenv").config();
@@ -50,5 +51,6 @@ app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopSearchRouter);
+app.use("/api/shop/review", shopReviewRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
